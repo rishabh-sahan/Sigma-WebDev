aaf803664d53467c8f03cd15e47a6f9202323c8f
Rishabh Jain <rishabhsahan@gmail.com>
2025-11-27 18:47:40 +0530

    Updated MongoDB

--- 43-44 MongoDB (Part 1 & Part 2)/44 Mongo/index.js
+++ 43-44 MongoDB (Part 1 & Part 2)/44 Mongo/index.js
@@ -16,5 +16,27 @@ const userSchema = new mongoose.Schema({
     age: Number
 });
 
-// const User = mongoose.model("User", userSchema);
-const Employee = mongoose.model("Employee", userSchema);
+const User = mongoose.model("User", userSchema);
+
+User.insertMany([
+    {name: "Abhishek", email: "abhishek@gmail.com", age: 22},
+    {name: "Gagan", email: "gagan@gmail.com", age: 25},
+    {name: "Vikas", email: "vikas@gmail.com", age: 28},
+]).then((res) => {
+    console.log(res);
+});
+
+// const user2 = new User({
+//     name: "Sahan",
+//     email: "sahan@gmail.com",
+//     age: 20
+// });
+
+// user2
+// .save()
+// .then((res) => {
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
